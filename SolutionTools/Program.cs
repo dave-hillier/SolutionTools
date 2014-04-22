4f28d1d8b9f78791363b6e0c3aa0a102f453776e
Dave Hillier <dave.hillier1@gmail.com>
2014-04-23 00:44:30 +0100

    Added command line package

--- SolutionTools/Program.cs
+++ SolutionTools/Program.cs
@@ -1,10 +1,58 @@
-﻿namespace SolutionTools
+﻿using System;
+using System.IO;
+using System.Linq;
+using CommandLine;
+using CommandLine.Text;
+
+namespace SolutionTools
 {
     public class Program
     {
+        public class ListSubOptions
+        {
+            [Option('a', "all", HelpText = "Help!")]
+            public bool All { get; set; }
+
+            [HelpOption]
+            public string GetUsage()
+            {
+                return HelpText.AutoBuild(this, current => HelpText.DefaultParsingErrorsHandler(this, current));
+            }
+        }
+
+        public class Options 
+        {
+            [VerbOption("list", HelpText = "List all dependencies for a project.")]
+            public ListSubOptions ListVerb { get; set; }
+
+            [VerbOption("sln", HelpText = "Create a solution.")]
+            public ListSubOptions SlnVerb { get; set; }
+
+            // TODO: auto generate
+
+            [HelpVerbOption]
+            public string GetUsage(string verb)
+            {
+                return HelpText.AutoBuild(this, verb);
+            }
+        }
+
         static void Main(string[] args)
         {
+            var options = new Options();
+            string invokedVerb;
+            object invokedVerbInstance;
 
+            if (CommandLine.Parser.Default.ParseArguments(args, options,
+              (verb, subOptions) =>
+              {
+                  // if parsing succeeds the verb name and correct instance
+                  // will be passed to onVerbCommand delegate (string,object)
+                  invokedVerb = verb;
+                  invokedVerbInstance = subOptions;
+              }))
+            {
+            }
         }
     }
 }
