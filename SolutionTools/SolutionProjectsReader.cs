c698f00e459de969a71ffef12ef92bdc298f079c
Dave Hillier <dave.hillier1@gmail.com>
2014-04-24 20:18:42 +0100

    Merged SolutionReader with SolutionProjectReader

--- SolutionTools/SolutionProjectsReader.cs
+++ SolutionTools/SolutionProjectsReader.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace SolutionTools
 {
@@ -16,8 +17,30 @@ namespace SolutionTools
         public IEnumerable<string> GetProjects()
         {
             var directory = Path.GetDirectoryName(_slnPath);
-            return SolutionReader.ReadAllProjects(_slnPath).
-                                  Select(fn => directory != null ? Path.Combine(directory, fn) : null).Where(fn => fn != null);
+            using (var f = File.OpenText(_slnPath))
+            {
+                var lines = ReadLines(f);
+                return ReadAllProjects(lines).
+                    Select(fn => directory != null ? Path.Combine(directory, fn) : null).
+                    Where(fn => fn != null);
+            }
+        }
+
+        private static IEnumerable<string> ReadLines(StreamReader f)
+        {
+            var lines = new List<string>();
+            while (!f.EndOfStream)
+                lines.Add(f.ReadLine());
+            return lines;
+        }
+
+        private static IEnumerable<string> ReadAllProjects(IEnumerable<string> lines)
+        {
+            var re = new Regex(@"Project\("".+?""\) = ""(.+?)"", ""(.+?)"", "".+?""");
+            return from line in lines
+                   let matches = re.Match(line) // TODO: use matches?
+                   where matches.Success && matches.Groups.Count == 3 && matches.Groups[1].Value != matches.Groups[2].Value
+                   select matches.Groups[2].Value;
         }
     }
 }
