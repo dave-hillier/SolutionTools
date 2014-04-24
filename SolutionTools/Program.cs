20fcbe0a11a95bab2375e597d982264951a0c5db
Dave Hillier <dave.hillier1@gmail.com>
2014-04-24 17:08:13 +0100

    added folderselector

--- SolutionTools/Program.cs
+++ SolutionTools/Program.cs
@@ -6,35 +6,6 @@ using System.Text.RegularExpressions;
 
 namespace SolutionTools
 {
-    public class FolderSelector
-    {
-        public static string GetSlnFolderByNamespace(string sln, string project)
-        {
-            var fn = Path.GetFileName(project);
-            var s = fn.Split('.');
-            return string.Join(".", s.Take(2));
-        }
-
-        public static string GetSlnFolder(string sln, string project)
-        {
-
-            var slnDir = Path.GetDirectoryName(sln);
-            var projectDir = Path.GetDirectoryName(project);
-            var next = Path.GetDirectoryName(projectDir);
-            if (next == slnDir)
-                return "";
-            // TODO: null checks
-
-            while (slnDir != next)
-            {
-                projectDir = next; 
-                next = Path.GetDirectoryName(projectDir);
-            }
-
-            return projectDir.Substring(slnDir.Length).Trim('\\');
-        }
-    }
-
     public class Program
     {
         private static readonly Regex TestFolderRegex = new Regex("[Tt]est");
